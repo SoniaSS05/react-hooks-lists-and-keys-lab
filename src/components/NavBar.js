ba9fc84ce221e72bb5da5c019aaf194e70ea5517
Sonia <serrano_sonia@hotmail.com>
2021-05-16 22:24:30 -0400

    Assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,12 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const linkjxs = links.map((addr) => {
+    return <a href={`#${addr}`} key={addr}>{addr}</a>;
+  });
+  return <nav>{/* display an <a> tag for each link here */}
+    {linkjxs}
+  </nav>;
 }
 
 export default NavBar;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,15 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  console.log('name ' + {name});
+ { /*const nMap = name.map((elem)=><span key={elem.id}>{elem.name}</span>)*/}
   return (
     <div className="project-item">
-      <h3>{name}</h3>
-      <p>{about}</p>
       <div className="technologies">
         {/* render a <span> for each technology in the technologies array */}
+          {name.map((elem)=><scan>{elem}</scan>)}
+        <scan>{about}</scan>
+        <scan>{technologies}</scan>
       </div>
     </div>
   );
