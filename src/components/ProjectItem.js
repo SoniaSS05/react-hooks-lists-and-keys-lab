1e2dc3425ea8d51c60bcf3d9040369bd20056f18
Sonia <serrano_sonia@hotmail.com>
2021-05-18 12:56:14 -0400

    Completed Assignment

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,15 +1,20 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
-  console.log('name ' + {name});
- { /*const nMap = name.map((elem)=><span key={elem.id}>{elem.name}</span>)*/}
+
   return (
     <div className="project-item">
       <div className="technologies">
         {/* render a <span> for each technology in the technologies array */}
-          {name.map((elem)=><scan>{elem}</scan>)}
-        <scan>{about}</scan>
-        <scan>{technologies}</scan>
+          <h1>{name}</h1>
+          <h3>{about}</h3>
+          {technologies.map((elem,index)=>
+              <div key={elem}><span>{elem}</span></div>
+          )}
+
+         
+
+
       </div>
     </div>
   );
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,15 +2,13 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-
+ 
   return (
     <div id="projects">
       <h2>My Projects</h2>
       <div id="project-list">{/* render ProjectItem components here */}
       {projects.map((item) => 
-          <ProjectItem  name ={projects.name} about = {projects.about} key = {projects.id} />)}
-          
-
+    <ProjectItem  name ={item.name} about = {item.about} key = {item.id} technologies = {item.technologies}/>)}
       </div>
     </div>
   );
